55af8b861cd734f2d6114d1cf45141513731a3fb
komal velukar <velukark@gmail.com>
2025-12-29 21:33:53 +0530

    App curd

---
+++ CurdApp3/src/main/java/com/example/CurdApp3/CurdApp3Application.java
@@ -0,0 +1,15 @@
+package com.example.CurdApp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdApp3Application.class, args);
+		
+		System.out.println("helloo");
+		}
+
+}
---
+++ CurdApp3/src/test/java/com/example/CurdApp3/CurdApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurdApp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
